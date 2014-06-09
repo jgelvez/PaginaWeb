30f284c332ba136e2c1b73595b402c2cc36f7fd8
jgelvez <gelvez30@gmail.com>
2014-06-09 15:29:21 -0300

    modificaciones sobre CargaOfertas

--- js/utiles.js
+++ js/utiles.js
@@ -1,23 +1,22 @@
 function apellidoValido(apellido) {
   return apellido.length > 3;
 }
- function 
  
  function ofertaValida(oferta){
 	return oferta.length > 4
  }
  
  function validarFecha(date1,date2){
-	valuesStart=date1.split("/");
-    valuesEnd=date2.split("/");
-
+	
+	valuesStart=date1.split("-");
+    valuesEnd=date2.split("-");
+	
     // Verificamos que la fecha no sea posterior a la actual
             var dateStart=new Date(valuesStart[2],(valuesStart[1]-1),valuesStart[0]);
+			
             var dateEnd=new Date(valuesEnd[2],(valuesEnd[1]-1),valuesEnd[0]);
-            if(dateStart>=dateEnd)
-            {
-                return 0;
-            }
-            return 1;
-	
+			return dateStart>=dateEnd;
+			
+            
+
  }
