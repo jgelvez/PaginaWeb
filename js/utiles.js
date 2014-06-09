f7e6aa8e26b5f5b997b593f1ed4e6e6222a4090f
julian <gelvez30@gmail.com>
2014-06-09 13:04:03 -0300

    modificaciones

---
+++ js/utiles.js
@@ -0,0 +1,23 @@
+function apellidoValido(apellido) {
+  return apellido.length > 3;
+}
+ function 
+ 
+ function ofertaValida(oferta){
+	return oferta.length > 4
+ }
+ 
+ function validarFecha(date1,date2){
+	valuesStart=date1.split("/");
+    valuesEnd=date2.split("/");
+
+    // Verificamos que la fecha no sea posterior a la actual
+            var dateStart=new Date(valuesStart[2],(valuesStart[1]-1),valuesStart[0]);
+            var dateEnd=new Date(valuesEnd[2],(valuesEnd[1]-1),valuesEnd[0]);
+            if(dateStart>=dateEnd)
+            {
+                return 0;
+            }
+            return 1;
+	
+ }
